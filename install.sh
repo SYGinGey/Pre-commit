fcc78b3c3cff3e37ae1b040daf0801570756a488
Sergei Yarygin <sergei.iarygin@b2broker.com>
2024-01-23 14:01:23 +0300

    fix

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 CHECKSTYLE_RELEASE="https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.7/checkstyle-10.12.7-all.jar"
 
-apt-get update && apt-get install -y ca-certificates wget unzip curl bash
+apt-get update && apt-get install -y ca-certificates wget unzip curl
 
 cd /opt
 
